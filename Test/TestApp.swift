d103d7644834c9fcb4f899fc6574d7a0e6d9cba5
Jakub Charvat <jakcharvat@gmail.com>
2021-01-29 12:01:38 +0300

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Jakub Charvat on 29.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
